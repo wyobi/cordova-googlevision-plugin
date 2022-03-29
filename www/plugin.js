fee4dbf94645400c902e0cf603014f855af34c30
Fanie Oosthuysen <fanie.oosthuysen@gmail.com>
2022-03-29 21:22:41 +0200

    Update plugin.js

--- www/plugin.js
+++ www/plugin.js
@@ -3,10 +3,9 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'GoogleVisionPlugin';
 
 var GoogleVisionPlugin = {
-  detectText: function(pattern, cb, errCb) {
-    return exec(cb, errCb, PLUGIN_NAME, 'detect', [pattern]);
+  detectText: function(pattern, detectOne, takePhoto, cb, errCb) {
+    return exec(cb, errCb, PLUGIN_NAME, 'detect', [pattern, detectOne, takePhoto]);
   }
 };
-
 
 module.exports = GoogleVisionPlugin;
